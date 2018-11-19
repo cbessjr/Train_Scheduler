bd07eeadaee34f7294dcde71869e5aac2e9d2912
Ender <cbessjr@gmail.com>
2018-11-19 14:37:11 -0700

    0230PM 111918 updated the schedule to connect to the database. Fixing the time entry and the posting of the new train to the page. Next will work on the conversion.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,45 @@
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHB3vVFL66mvsZaV5XeTYXqEk46al9lDc",
+    authDomain: "ender-a6257.firebaseapp.com",
+    databaseURL: "https://ender-a6257.firebaseio.com",
+    projectId: "ender-a6257",
+    storageBucket: "ender-a6257.appspot.com",
+    messagingSenderId: "182360977035"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+
+
 $("#add-train-btn").on("click", function () {
     event.preventDefault();
 
-    var employeeName = $("#employee-name-input").val().trim();
-    var employeeRole = $("#role-input").val().trim();
-    var employeeStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
-    var employeeRate = $("#rate-input").val().trim();
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainStart = moment($("#start-input").val().trim(), "HH:MM").format("X");
+    var trainFrequency = $("#frequency-input").val().trim();
 
-    var newEmployee = {
-        name: employeeName,
-        role: employeeRole,
-        start: employeeStart,
-        rate: employeeRate
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        start: trainStart,
+        frequency: trainFrequency
     };
 
-    database.ref().push(newEmployee);
+    database.ref().push(newTrain);
 
-    console.log(newEmployee.name);
-    console.log(newEmployee.role);
-    console.log(newEmployee.start);
-    console.log(newEmployee.rate);
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
 
-    alert("Successfully added Employee")
+    alert("Successfully added Train")
 
     $("#employee-name-input").val("")
     $("#role-input").val("")
@@ -33,18 +52,18 @@ database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot);
 
     var name = childSnapshot.val().name
-    var role = childSnapshot.val().role
+    var destination = childSnapshot.val().destination
     var start = childSnapshot.val().start
     var rate = childSnapshot.val().rate
-    var startPretty = moment.unix(start).format("MM/DD/YYYY");
-    console.log(startPretty);
+    var startTime = moment.unix(start).format("MM/DD/YYYY");
+    console.log(startTime);
     var mosWorked = moment().diff(moment(start, "X"), "months");
     var billed = mosWorked * rate;
 
     var newRow = $("<tr>").append(
         $("<td>").text(name),
-        $("<td>").text(role),
-        $("<td>").text(startPretty),
+        $("<td>").text(destination),
+        $("<td>").text(startTime),
         $("<td>").text(mosWorked),
         $("<td>").text("$" + rate),
         $("<td>").text("$" + billed)
